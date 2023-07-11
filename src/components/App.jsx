4110952fc9da0b65437ba19f003f5283cbb2da6f
Leo007ua <leo.oliinyk@ukr.net>
2023-07-11 15:10:08 +0300

    qwe

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from 'components/Profile/Profile';
 import { FriendList } from './FriendList/FriendList';
 // import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import user from 'FilesJson/user.json';
-// import data from 'FilesJson/data.json';
+import data from 'FilesJson/data.json';
 import friends from 'FilesJson/friends.json';
 // import transactions from 'FilesJson/transactions.json';
-// import { Statistics } from './Statistics/Statistics';
+import { Statistics } from './Statistics/Statistics';
 
 export const App = () => {
   return (
@@ -28,7 +28,7 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={data} /> */}
+      <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       {/* <TransactionHistory items={transactions} /> */}
     </div>
